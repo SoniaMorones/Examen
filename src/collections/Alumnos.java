45d18496d0f341d0483855b3ca65b5c8cf19fc5b
SoniaMorones <soniamorones8@gmail.com>
2023-03-25 10:09:45 -0600

    prueba

---
+++ src/collections/Alumnos.java
@@ -0,0 +1,27 @@
+package collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Alumnos {
+
+    public static void main(String[] args) {
+        
+        Set alumnos = new HashSet();
+        alumnos.add("Juan");
+        alumnos.add("Leonardo");
+        alumnos.add("Patricia");
+        alumnos.add("Diego");
+        alumnos.add("Elizabeth");
+        alumnos.add("Maria");
+        alumnos.add("Rosa");
+        alumnos.add("Luis");
+        alumnos.add("Mariel");
+        alumnos.add("Enrique");
+
+        System.out.println("Listado de Alumnos: " );
+        System.out.println(alumnos);
+
+}
+
+
+}
